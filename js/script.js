590fd43c514d664f32a22421446001bbee2410ef
Dawqss <Dawidwyrkowski@gmail.com>
2016-10-27 07:45:29 +0200

    konflikt

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 var a = prompt ('podaj a: ');
 console.log('a wynosi: '+a)
-<<<<<<< HEAD
 var b = prompt ('podaj b: ');
-=======
-var b = prompt ('podab b: ');
->>>>>>> 904020a305f62fb2dc74f4b596d71bf76cfc3b61
 console.log('b wynosi: '+b)
 var value = (a * a) + (2 * a * b) - (b * b);
 console.log('wartość równania: '+value)
