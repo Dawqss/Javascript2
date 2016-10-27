904020a305f62fb2dc74f4b596d71bf76cfc3b61
Dawid Wyrkowski <dawidwyrkowski@gmail.com>
2016-10-27 07:20:17 +0200

    exscercise 8_5

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var a = prompt ('podaj a: ');
+console.log('a wynosi: '+a)
+var b = prompt ('podab b: ');
+console.log('b wynosi: '+b)
+var value = (a * a) + (2 * a * b) - (b * b);
+console.log('wartość równania: '+value)
+
+alert(+value)
+
+if (+value > 0) {
+	console.log('wynik dodatni')
+}
+else if (+value < 0) {
+	console.log('wynik ujemny')
+}
+else {
+	console.log('wynik wynosi zero')
+}
