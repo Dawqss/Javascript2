9cd5170d2a12ce3691990f8183de5ceff4d7aacd
Dawid Wyrkowski <dawidwyrkowski@gmail.com>
2016-10-28 14:39:02 +0200

    #1 issue corrected

--- js/script.js
+++ js/script.js
@@ -1,20 +1,16 @@
 var a = prompt ('podaj a: ');
 console.log('a wynosi: '+a)
-<<<<<<< HEAD
 var b = prompt ('podaj b: ');
-=======
-var b = prompt ('podab b: ');
->>>>>>> 904020a305f62fb2dc74f4b596d71bf76cfc3b61
-console.log('b wynosi: '+b)
+console.log('b wynosi: '+b);
 var value = (a * a) + (2 * a * b) - (b * b);
-console.log('wartość równania: '+value)
+console.log('wartość równania: '+value);
 
-alert(+value)
+alert(value);
 
-if (+value > 0) {
+if (value > 0) {
 	console.log('wynik dodatni')
 }
-else if (+value < 0) {
+else if (value < 0) {
 	console.log('wynik ujemny')
 }
 else {
